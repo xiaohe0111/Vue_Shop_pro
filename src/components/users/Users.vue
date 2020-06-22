8d51ccf5921b2983ad0232bceda6674a75168472
xiaohe <xiaohe@163.com>
2020-06-22 10:28:58 +0800

    完成用户的编辑和删除功能

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -33,7 +33,28 @@
             <el-switch v-model="slotScope.row.mg_state" @change="userStateChanged(slotScope.row)"></el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="scope">
+            <!-- 编辑按钮 -->
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-edit"
+              @click="showEditDialog(scope.row)"
+            ></el-button>
+            <!-- 删除按钮 -->
+            <el-button
+              type="danger"
+              size="mini"
+              icon="el-icon-delete"
+              @click="removeUserById(scope.row.id)"
+            ></el-button>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页区域 -->
       <el-pagination
@@ -67,9 +88,32 @@
         <el-button type="primary" @click="addUsers">确 定</el-button>
       </span>
     </el-dialog>
+    <!-- 修改用户的区域 -->
+    <el-dialog title="编辑用户" :visible.sync="showDialogVisible" width="50%">
+      <el-form
+        ref="editFormRef"
+        :rules="editFormRules"
+        :model="editForm"
+        label-width="70px"
+        @close="editFormClosed"
+      >
+        <el-form-item label="用户名">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="showDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUser">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
-
 <script>
 export default {
   data() {
@@ -109,12 +153,14 @@ export default {
       userList: [],
       total: 0,
       addDialogVisible: false,
+      showDialogVisible: false,
       addForm: {
         username: '',
         password: '',
         email: '',
         mobile: ''
       },
+      // 添加用户的表单验证规则
       addFormRules: {
         username: [
           { required: true, message: '请输入用户名称', trigger: 'blur' },
@@ -124,6 +170,19 @@ export default {
           { required: true, message: '请输入密码', trigger: 'blur' },
           { min: 6, max: 15, message: '长度在6 到 15 个字符', trigger: 'blur' }
         ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机好吗', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      },
+      // 编辑用户的信息
+      editForm: {},
+      // 修改用户的表单验证规则
+      editFormRules: {
         email: [
           { required: true, message: '请输入邮箱', trigger: 'blur' },
           { validator: checkEmail, trigger: 'blur' }
@@ -198,6 +257,73 @@ export default {
         // 重新获取用户列表数据
         this.getUserList()
       })
+    },
+    // 点击修改按钮触发的事件
+    showEditDialog(row) {
+      console.log('ok')
+      this.editForm = row
+      console.log(row)
+      this.showDialogVisible = true
+    },
+    // 监听修改用户对话框的关闭事件
+    editFormClosed() {
+      this.$refs.editFormRef.resetFields()
+    },
+    // 点击修改按钮修改用户事件
+    editUser() {
+      // 先对表单进行预校验
+      this.$refs.editFormRef.validate(async valid => {
+        console.log(valid)
+        // 如果校验失败直接return掉
+        if (!valid) return
+        // 校验成功就发送请求
+        const { data: res } = await this.$http.put(
+          'users/' + this.editForm.id,
+          this.editForm
+        )
+        console.log(res)
+        // 对状态码进行判断
+        if (res.meta.status !== 200) return this.$message.error('修改用户失败')
+        // 否则就是修改用户成功
+        this.$message.success('添加用户成功')
+        // 隐藏添加用户的对话框
+        this.showDialogVisible = false
+        // 重新获取用户列表数据
+        this.getUserList()
+      })
+    },
+    // 当点击删除按钮,删除当前的用户
+    async removeUserById(id) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该用户, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err) // 这里是捕获错误对象的
+      // 返回的结果:如果点击的是取消按钮则返回的是cancle,如果点击的是确定按钮则返回的是confirm
+      console.log(confirmResult)
+      // 现在对返回结果进行判断
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('您已取消了删除用户')
+      }
+      // 否则就是确定删除用户
+      // 发送删除用户的请求
+      const { data: res } = await this.$http.delete('users/' + id)
+      console.log(res)
+      // 现在用删除用户的状态进行判断
+      if (res.meta.status !== 200) return this.$message.error('用户删除失败')
+      this.$message.success('用户删除成功')
+      // 对当前的页面进行判断
+      if (this.userInfo.pagenum !== 1) {
+        if (this.userList.length === 1) {
+          this.userInfo.pagenum -= 1
+        }
+      }
+      // 重新获取用户列表
+      this.getUserList()
     }
   }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Submenu, Menu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Pagination, Dialog } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Submenu, Menu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Pagination, Dialog, Tooltip, MessageBox } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -22,5 +22,7 @@ Vue.use(TableColumn)
 Vue.use(Switch)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tooltip)
 // 全局挂载message
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
